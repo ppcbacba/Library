2ffa4edccdd3e26d008c9abb796d316e60320632
qlzcl <qlzcl@163.com>
2020-08-28 20:39:53 +0800

    api/authors/???/books

---
+++ Library.API/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public BookController(IBookRepository bookRepository,IAuthorRepository authorRepository)
+        {
+            BookRepository = bookRepository;
+            AuthorRepository = authorRepository;
+        }
+
+        public IAuthorRepository AuthorRepository { get; }
+
+        public IBookRepository BookRepository { get;  }
+
+        [HttpGet]
+        public ActionResult<List<BookDto>> GetBooks(Guid authorId)
+        {
+            if (!AuthorRepository.IsAuthorExists(authorId))
+            {
+                return NotFound();
+
+            }
+
+            return BookRepository.GetBooksForAuthor(authorId).ToList();
+        }
+    }
+}
